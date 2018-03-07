0d9d9d3a35f429397f8a3b3ceb79e18d460a2e30
Robson Farias <fariasrobson.r@gmail.com>
2018-03-06 18:55:29 -0800

    crud events get create delete upload eventst

--- server.js
+++ server.js
@@ -1,45 +1,71 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
-
 const port = 3000
-app.use(bodyParser.json())
 
-let events = [
-  {uid:1},
-  {title:'web dev week'},
-  {description:'conference'},
-  {date:'03/06/18'},
-  {time:'6:00pm'},
-  {duration:'2 hours'}
-]
 
-app.get('/events', function (req, res) {
+let events = [{
+    id: 0,
+    title: "dev week",
+    description: "conference",
+    date: "03-06-18",
+  },
+  {
+    id: 1,
+    title: "tech meetups",
+    description: "meetup",
+    date: "04-07-19",
+  }]
 
-  console.log('request info');
+app.get('/events', (req, res) => {
+  console.log('get event')
+    res.send(events)
+})
+
+app.get("/events/:id", (req, res) => {
+    if((req.params.id < 0 ) || (req.params.id > events.length  - 1) || isNaN(req.params.id)) {
+      console.log("error not found")
+      res.sendStatus(404)
+    } else {
+      res.send(events[req.params.id])
+    }
+})
+
+app.post('/events', (req, res) => {
+  let newEvent = {
+    id: events.length,
+    title: "new event",
+    description: "add event",
+    date: "new date"
+  }
+  events.push(newEvent)
   res.send(events)
 })
 
-app.post('/events/uid', function (req, res) {
-  console.log('events have been posted', port);
-  res.send({events:uid})
+app.delete('/events/:id', (req, res) => {
+  events.forEach((element, index) => {
+    if (Number(element.id) === Number(req.params.id)) {
+      events.splice(req.params.id, 1)
+      console.log("events>>>>>", events);
+      res.send(events)
+    }
+  })
+  res.sendStatus(404)
 })
 
-app.post('/events', function (req, res) {
-
+app.put('/events/:id', (req, res) => {
+  events.forEach((element, index) => {
+    if (Number(element.id) === Number(req.params.id)) {
+      events[index] = {
+        id: Number(req.params.id),
+        title: "free beer",
+        description: "happy hour",
+        date: "tomorrow"
+      }
+      res.send(events[req.params.id])
+    }
+  })
+  res.sendStatus(404)
 })
 
-app.put('/events', function (req, res) {
-  console.log('events have been put', port);
-  res.send(events)
-})
-
-app.delete('/events', function (req, res) {
-  console.log('events have been delete', port);
-  res.send(events)
-})
-
-
-app.listen(port, function (){
-  console.log('server is up', port);
-})
+app.listen(port, () =>
+  console.log('server is up'))
